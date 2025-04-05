8bb53dd42f4f70a9b39d08b732785b51ea88283b
raimurzin <aimurzin05@mail.ru>
2025-04-06 00:15:16 +0500

    fix role person

--- lab2/university/roles/person.hpp
+++ lab2/university/roles/person.hpp
@@ -1,23 +1,13 @@
 #pragma once
 #include <iostream>
 
-enum class Gender {
-    Man, // Мужчина
-    Woman // Женщина
-};
-
-enum class ActivismType {
-    Sportsmen, // Спортсмен
-    HostelVolunteer // Помощник в общежитии
-};
-
 class Person {
 public:
     Person() {
         std::cout << "Created Unknown Person\n";
     }
-    Person(uint age, uint gender, std::string name) : age(age), gender(gender), name(name) {}
-    Person(const Person& p) : age(p.age), gender(p.gender), name(p.name) {}
+    Person(uint age, std::string name) : age(age), name(name) {}
+    Person(const Person& p) : age(p.age), name(p.name) {}
     Person& operator=(const Person& p) { 
         if (this != &p) {
             Person temp(p);
@@ -34,12 +24,10 @@ private:
     void swap(Person& first, Person& second) noexcept {
         using std::swap;
         swap(first.age, second.age);
-        swap(first.gender, second.gender);
         swap(first.name, second.name);
     }
 private:
     uint age;
-    uint gender;
     std::string name;
 };
 
