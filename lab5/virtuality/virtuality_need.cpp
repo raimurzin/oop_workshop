c4cfcf6c838fe8b06be497a9d150221b6d523ebd
raimurzin <aimurzin05@mail.ru>
2025-04-09 05:23:11 +0500

    describe virtuality need

--- lab5/virtuality/virtuality_need.cpp
+++ lab5/virtuality/virtuality_need.cpp
@@ -2,12 +2,18 @@
 
 namespace overlapping {
     struct Base {
+        Base() { std::cout << "Created Base and allocated sources\n"; }
+        ~Base() { std::cout << "Destroyed Base and deallocated sources\n"; }
+        
         void write_smth() const {
             std::cout << "It's base\n";
         }
     };
 
     struct Derived : public Base {
+        Derived() { std::cout << "Created Derived and allocated sources\n"; }
+        ~Derived() { std::cout << "Destroyed Derived and deallocated sources\n"; }
+        
         void write_smth() const {
             std::cout << "It's derived\n";
         }
@@ -16,27 +22,32 @@ namespace overlapping {
 
 namespace virtuality {
     struct Base {
+        Base() { std::cout << "Created Base and allocated sources\n"; }
+        virtual ~Base() { std::cout << "Destroyed Base and deallocated sources\n"; }
+
         virtual void write_smth() const {
             std::cout << "It's base\n";
         }
     };
 
     struct Derived : public Base {
-        virtual void write_smth() const {
+        Derived() { std::cout << "Created Derived and allocated sources\n"; }
+        virtual ~Derived() { std::cout << "Destroyed Derived and deallocated sources\n"; }
+
+        virtual void write_smth() const override {
             std::cout << "It's derived\n";
         }
     };
 }
 
 int main() {
-    {
-        overlapping::Base* b = new overlapping::Derived();
-        b->write_smth();
-        delete b;
-    }
-    {
-        virtuality::Base* b = new virtuality::Derived();
-        b->write_smth();
-        delete b;
-    }
+    std::cout << "Without using virtuality:\n";
+    overlapping::Base* b1 = new overlapping::Derived();
+    b1->write_smth();
+    delete b1;
+
+    std::cout << "\nWith using virtuality:\n";
+    virtuality::Base* b2 = new virtuality::Derived();
+    b2->write_smth();
+    delete b2;
 }
